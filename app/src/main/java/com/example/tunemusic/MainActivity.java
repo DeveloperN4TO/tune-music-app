f0b97b090c039776743e9d22d56f6ac165285075
sergio rodrigues <sergio.n4todev@gmail>
2023-02-05 01:34:21 -0300

    Nova atualização
    Foi implementado
    funcionalidade ao botão volume + codificação
    imageView indicando o SeekBar como volume

--- app/src/main/java/com/example/tunemusic/MainActivity.java
+++ app/src/main/java/com/example/tunemusic/MainActivity.java
@@ -2,23 +2,66 @@ package com.example.tunemusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer medidiaplayer;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inicializarSeekBar();
+
         medidiaplayer = MediaPlayer.create(getApplicationContext(),
                 R.raw.mariamarcal_tempodecantar);
 
     }
+    private void inicializarSeekBar(){
+
+        seekVolume = findViewById(R.id.seekVolume);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekVolume.setMax(volumeMaximo);
+        seekVolume.setProgress( volumeAtual );
+
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume( AudioManager.STREAM_MUSIC, i, 0);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+    }
+
 
     public void executorSom(View view) {
 
